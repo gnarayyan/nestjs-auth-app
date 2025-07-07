965392937e47d5d48b0965691baac7af2e3a1439
Narayan Gautam <46322720+gnarayyan@users.noreply.github.com>
2025-07-07 19:58:19 +0545

    fix: correct module import and enhance main application setup with global validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strips away properties that are not defined in the DTO
+      forbidNonWhitelisted: true, // Throws an error if non-whitelisted properties are sent
+      transform: true, // Automatically transform payloads to DTO instances
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on http://localhost:3000`);
 }
 bootstrap();
